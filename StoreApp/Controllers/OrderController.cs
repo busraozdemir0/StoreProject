c917b97b5d5e918abd4b004ca49a74cf83390d1d
Büşra <98536303+busraozdemir0@users.noreply.github.com>
2023-08-15 12:05:12 +0300

    Sipariş verdirme yapıldı (complete_order)

---
+++ StoreApp/Controllers/OrderController.cs
@@ -0,0 +1,39 @@
+using Entities.Models;
+using Microsoft.AspNetCore.Mvc;
+using Services.Contracts;
+
+namespace StoreApp.Controllers
+{
+    public class OrderController:Controller
+    {
+        private readonly IServiceManager _manager;
+        private readonly Cart _cart;
+
+        public OrderController(IServiceManager manager, Cart cart)
+        {
+            _manager = manager;
+            _cart = cart;
+        }
+        public ViewResult Checkout()=> View(new Order());
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]  // sahteciliği önlemek adına
+        public IActionResult Checkout([FromForm] Order order)
+        {
+            if(_cart.Lines.Count()==0)
+            {
+                ModelState.AddModelError("","Sorry, your cart is empty."); // Sepet boşsa hata mesajı dönsün
+            }
+            if(ModelState.IsValid)
+            {
+                order.Lines=_cart.Lines.ToArray();
+                _manager.OrderService.SaveOrder(order);
+                _cart.Clear();
+                return RedirectToPage("/Complete",new{OrderId=order.OrderId});
+            }
+            else{
+                return View();
+            }
+        }
+    }
+}
